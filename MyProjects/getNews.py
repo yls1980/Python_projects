f1ae6a93421c67614ed4a2b9a1ac0e16c850b27a
Yaroslav Skorkin <y_skorkin@mail.ru>
2022-11-25 00:08:01 +0300

    commit 20221125

--- MyProjects/getNews.py
+++ MyProjects/getNews.py
@@ -11,7 +11,58 @@ from selenium import webdriver
 from selenium.common import InvalidSelectorException
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import os
+import time
+import psycopg2
+from googletrans import Translator
+
+def set_conn():
+    conn = psycopg2.connect(dbname='zorrodb', user='zorropg',
+                            password='rjnjhsq1980', host='168.232.165.10')
+    return conn
+
+
+def translate(stext,lfrom, lto):
+    translator = Translator()
+    translated = translator.translate(stext,  src=lfrom, dest=lto)
+    return translated.text
+
+def ins_to_db(conn, rus_text,origin_text, eng_text, link, surl, lang):
+    sorigin_text = origin_text
+    bload = True
+    if (rus_text!='' and eng_text==''):
+        eng_text = translate(rus_text, 'ru','en')
+        sorigin_text = rus_text
+    elif (rus_text=='' and eng_text!=''):
+        rus_text = translate(eng_text, 'en', 'ru')
+        sorigin_text = eng_text
+    elif (rus_text != '' and eng_text !=''):
+        sorigin_text = rus_text
+    elif (rus_text == '' and eng_text =='' and origin_text!=''):
+        rus_text = translate(eng_text, lang, 'ru')
+        eng_text = translate(eng_text, lang, 'en')
+        sorigin_text = origin_text
+    else:
+        bload = False
+    if bload:
+        cur = conn.cursor()
+        cur.execute('select public.ins_t_news_data(%s, %s, %s, %s, %s) as val', (rus_text,sorigin_text, eng_text, link, surl))
+        conn.commit()
+        cur.close()
+
+
+def ins_text()
+    conn = set_conn();
+    ins_to_db(conn, 'sdfd','sdfd','sdfd','sdfd','sdfd')
+    conn.close()
+
+#prus_text text,
+#     porigin_text text,
+#     peng_text text,
+#     plink text,
+#     psurl text)
+
+
+
 
 def bbs():
     url = 'https://www.bbc.com/news'
@@ -181,8 +232,217 @@ def techcrunch():
             slink = url + slink
         stext = x.findNext('div', class_='post-block__content').text.strip()
         print(stext, slink)
-#<header class="post-block__header"><div class="article__primary-category"><a class="article__primary-category__link gradient-text gradient-text--green-gradient" href="/category/venture/">Venture</a></div><h2 class="post-block__title"><a class="post-block__title__link" href="/2022/11/18/drive-capitals-investors-hit-a-fork-in-the-road/">Drive Capital’s investors reach a fork in the road</a></h2><div class="post-block__meta"><div class="river-byline"><span class="river-byline__authors"><span><a aria-label="Posts by Connie Loizos" href="/author/connie-loizos/">Connie Loizos</a></span></span><div class="river-byline__full-date-time__wrapper"><time class="river-byline__full-date-time" datetime="2022-11-19T05:29:19">8:29 AM GMT+3<span class="full-date-time__separator">•</span>November 19, 2022</time></div></div></div></header>
-#<article class="post-block post-block--image post-block--unread river-enter-active"><header class="post-block__header"><div class="article__primary-category"><a class="article__primary-category__link gradient-text gradient-text--green-gradient" href="/category/venture/">Venture</a></div><h2 class="post-block__title"><a class="post-block__title__link" href="/2022/11/18/drive-capitals-investors-hit-a-fork-in-the-road/">Drive Capital’s investors reach a fork in the road</a></h2><div class="post-block__meta"><div class="river-byline"><span class="river-byline__authors"><span><a aria-label="Posts by Connie Loizos" href="/author/connie-loizos/">Connie Loizos</a></span></span><div class="river-byline__full-date-time__wrapper"><time class="river-byline__full-date-time" datetime="2022-11-19T05:29:19">8:29 AM GMT+3<span class="full-date-time__separator">•</span>November 19, 2022</time></div></div></div></header><div class="post-block__content">Drive Capital was founded by two former Sequoia Capital Partners looking to start anew in the Midwest. But investors in the Columbus, Oh.-based firm have had a bumpy ride of late, and according to ...</div><footer class="post-block__footer"><figure class="post-block__media"><picture><source srcset="https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=100&amp;h=53&amp;crop=1 100w" media="(max-width: 380px)"><source srcset="https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=200&amp;h=107&amp;crop=1 200w" media="(max-width: 575px)"><source srcset="https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=430&amp;h=230&amp;crop=1 430w" media="(min-width: 576px)"><img alt="" sizes="(min-width: 1024px) 430px, 100vw" src="https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=430&amp;h=230&amp;crop=1" srcset="https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=100&amp;h=53&amp;crop=1 100w, https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=200&amp;h=107&amp;crop=1 200w, https://techcrunch.com/wp-content/uploads/2016/11/gettyimages-157429059.jpg?w=430&amp;h=230&amp;crop=1 430w"></picture></figure></footer></article>
+
+def meduza():
+    url = 'https://meduza.io/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('a', class_='Link-root Link-isInBlockTitle')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        datas_txt = x.find_all('span')
+        texts =[]
+        for j, y in enumerate(datas_txt):
+            try:
+                texts.append(y.text.strip())
+            except:
+                pass
+            stext = '. '.join(texts)
+            if stext!='':
+                print(stext, slink)
+
+def kommersant():
+    url = 'https://www.kommersant.ru/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('a', class_='uho__link uho__link--overlay')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink)
+
+def unian():
+    url = 'https://www.unian.net/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('a', class_='list-news__title')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink)
+    datas = soup.find_all('a', class_='main-publication__text')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink)
+
+def svpressa():
+    url = 'https://svpressa.ru/?utm_source=vsesmi_online'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('a', class_='b-article__title')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink)
+
+def iz():
+    browser = webdriver.Chrome()
+    url = "https://iz.ru/?utm_source=vsesmi_online"
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    datas = soup.find_all('a', class_='two-infographic-block__item__inside__info')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.findNext('span').text.strip()
+        if stext != '':
+            print(stext, slink)
+    datas = soup.find_all('a', class_='node__cart__item__inside url-box')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        datas_txt = x.find_all('div', class_="five-news-blocks__list__item__info__title text-style1 big_text title-box")+\
+                    x.find_all('div', class_='quote-content quote-box')+\
+                    x.find_all('div', class_='node__cart__item__inside__info__title small-title-style1 title-box')
+
+        texts = []
+        for j, y in enumerate(datas_txt):
+            try:
+                texts.append(y.findnext("span").text.strip())
+            except:
+                pass
+            try:
+                texts.append(y.findnext("div").text.strip())
+            except:
+                pass
+            try:
+                texts.append(y.text.strip())
+            except:
+                pass
+            stext = '. '.join(texts)
+            if stext != '':
+                print(stext, slink)
+    browser.close()
+
+def rg():
+    url = 'https://rg.ru/?utm_source=vsesmi_online'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('a', class_='ItemOfListStandard_title__eX0Jw')+\
+            soup.find_all('a', class_='NewsFeedItemAccent_link__B7ive')+ \
+            soup.find_all('a', class_='PageIndexContentSecondSpiegelListPattern_title__lWoOM') + \
+            soup.find_all('a', class_='NewsFeedItem_link__4XiJ_')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        texts = []
+        try:
+            texts.append(x.findnext("span").text.strip())
+        except:
+            pass
+        try:
+            texts.append(x.findnext("div").text.strip())
+        except:
+            pass
+        try:
+            texts.append(x.text.strip())
+        except:
+            pass
+        stext = '. '.join(texts)
+        if stext != '':
+            print(stext, slink)
+
+def ukr():
+    browser = webdriver.Chrome()
+    url = "https://www.ukr.net/news/main.html?utm_source=vsesmi_online"
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    datas = soup.find_all('a', class_='im-tl_a')
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink,'ukr')
+    browser.close()
+
+def hotnews():
+    browser = webdriver.Chrome()
+    url = "https://www.hotnews.ro/"
+    browser.get(url)
+    time.sleep(3)
+    try:
+        button = browser.find_element(By.ID,'onetrust-accept-btn-handler')
+        button.click()
+    except InvalidSelectorException as e:
+        print('Не найдена кнопка активации')
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    datas = soup.find_all('a', class_="snip")
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.text.strip()
+        print(stext, slink, 'rom')
+    browser.close()
+
+def gazeta():
+    browser = webdriver.Chrome()
+    url = "https://www.gazeta.pl"
+    browser.get(url)
+    time.sleep(3)
+    try:
+        button = browser.find_element(By.ID,'onetrust-accept-btn-handler')
+        button.click()
+    except InvalidSelectorException as e:
+        print('Не найдена кнопка активации')
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    datas = soup.find_all('a', class_="article")
+    for i, x in enumerate(datas):
+        slink = x.attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        stext = x.findNext('div',class_="article__title").text.strip()
+        print(stext, slink, 'pl')
+    browser.close()
+
+def svpressa():
+    url = 'https://oxu.az/?utm_source=vsesmi_online'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    datas = soup.find_all('div', class_='news-i')
+    for i, x in enumerate(datas):
+        slink = x.findNext('a').attrs['href']
+        if (slink[0] == '/'):
+            slink = url + slink;
+        try:
+            stext = x.findNext('div', class_="title").text.strip()
+        except:
+            pass
+        try:
+            stext = x.findNext('div').text.strip()
+        except:
+            pass
+        try:
+            stext = x.text.strip()
+        except:
+            pass
+        if stext != '':
+            print(stext, slink, 'az')
 
 #bbs()
 #abcnews()
@@ -191,4 +451,15 @@ def techcrunch():
 #mirror()
 #washingtonpost()
 #foxnews()
-techcrunch()
+#techcrunch()
+#meduza()
+#kommersant()
+#unian()
+#svpressa()
+#iz()
+#rg()
+#ukr()
+#hotnews()
+#gazeta()
+#svpressa()
+
